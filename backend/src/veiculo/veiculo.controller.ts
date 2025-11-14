0aa554f161aded23a882171a1052920e1cb9f1a3
Pedro Costa <pedro4costa@outlook.com>
2025-11-14 11:53:27 -0300

    Projeto inicial

---
+++ backend/src/veiculo/veiculo.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { VeiculoService } from './veiculo.service';
+
+@Controller('veiculo')
+export class VeiculoController {
+    constructor(private readonly veiculoService: VeiculoService) {}
+
+    @Get()
+    findAll() {
+        return this.veiculoService.findAll();
+    }
+
+    @Get('nomes')
+        async listarNomes() {
+        return this.veiculoService.listarNomes();
+  }
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+async function getVeiculos() {
+  const res = await fetch("http://localhost:3000/veiculo/nomes", {
+    cache: "no-store"
+  });
+
+  return res.json();
+}
+
+export default async function Home() {
+  const veiculos = await getVeiculos();
+  
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Lista de Veículos</h1>
+      <ul>
+        {veiculos.map((v: any, index: number) => (
+          <li key={index}>{v.modelo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
